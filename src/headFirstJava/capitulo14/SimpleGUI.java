0869b1e5660c0aec45c5bc4d8d75fa0b7884afc7
Nelnota <291840536+Nelnota@users.noreply.github.com>
2026-06-28 12:57:50 +0200

    Colocando o botao na tela

--- src/headFirstJava/capitulo14/SimpleGUI.java
+++ src/headFirstJava/capitulo14/SimpleGUI.java
@@ -1,7 +1,14 @@
 package headFirstJava.capitulo14;
+import javax.swing.JFrame;
+import javax.swing.JButton;
 
 public class SimpleGUI {
     public static void main(String[] args) {
-        
+         JFrame frame = new JFrame();
+    JButton button = new JButton("Click me");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.getContentPane().add(button);
+    frame.setSize(300, 300);
+    frame.setVisible(true);
     }
 }
